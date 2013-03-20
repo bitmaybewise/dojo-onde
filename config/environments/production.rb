7c3cfcef58d54e0fc0ab2dc97d58cda175564a00
Hercules Merscher <hlmerscher@gmail.com>
2013-03-20 10:43:11 -0300

    ajeitando assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,10 @@ Dojoaonde::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  # Compressor to use
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor  = :uglifier
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
