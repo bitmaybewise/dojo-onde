cff44db4c5becf117be9f5d32fac91100d406da7
Hercules Merscher <hlmerscher@gmail.com>
2013-05-07 00:00:56 -0300

    impede registro de data anterior somente na criação

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -8,7 +8,7 @@ class Dojo < ActiveRecord::Base
   validates_presence_of :day
   validates_presence_of :local
   validates_presence_of :gmaps_link
-  validate :day_cannot_be_in_the_past
+  validate :day_cannot_be_in_the_past, on: :create
 
   def self.happened
     where("day < ? ", Date.today).order("day DESC")
@@ -24,7 +24,7 @@ class Dojo < ActiveRecord::Base
 
   private
   def day_cannot_be_in_the_past
-    if !day.blank? and day < Date.today
+    if !day.blank? && day < Date.today
       errors.add(:day, "anterior não é permitido")
     end
   end
