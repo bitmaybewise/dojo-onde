07799eb569fe36390108f32d92c754e8aef5bf69
Hercules Merscher <hlmerscher@gmail.com>
2013-06-22 09:49:55 -0300

    coveralls antes de tudo no test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
+require 'coveralls'
+Coveralls.wear!('rails')
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
 require 'turn'
-require 'coveralls'
-
-Coveralls.wear!('rails')
 
 DatabaseCleaner.strategy = :truncation
 
