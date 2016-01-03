eb48188a88efd365ee39231efead35247a9fffc2
Hercules Merscher <hlmerscher@gmail.com>
2016-01-03 15:49:31 -0200

    simplecov formatters new configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'simplecov'
 require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatters = [
   Coveralls::SimpleCov::Formatter,
   SimpleCov::Formatter::HTMLFormatter
 ] 
